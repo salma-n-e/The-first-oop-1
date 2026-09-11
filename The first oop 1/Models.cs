28ec14a18de0abfeecc019d827489289e158777d
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-11 03:26:26 +0300

    DeliveryAddress

--- The first oop 1/Models.cs
+++ The first oop 1/Models.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using The_first_oop_1;
+
+namespace The_first_oop_1
+{
+    public struct DeliveryAddress
+    {
+
+        //1  DeliveryAddress struct
+
+
+        public string city;
+        public string street;
+        public int BuildingNumber;
+        public DeliveryAddress(string City, string street, int buildingNumber)
+        {
+            this.city = city;
+            this.street = street;
+            this.BuildingNumber = buildingNumber;
+        }
+        public string GetFullAddress()
+        {
+            return $"{city}, {street}, {BuildingNumber}";
+
+        }
+    }
+}
