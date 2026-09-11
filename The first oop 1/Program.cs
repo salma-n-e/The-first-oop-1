d7cf03c45e192613656fc9e5b6a9bedbcc02a18d
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-11 03:13:11 +0300

    Q 1

--- The first oop 1/Program.cs
+++ The first oop 1/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using The_first_oop_1;
+
+namespace The_first_oop_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Q1 
+            // a) is a struct (Value Type) Modifying the copy will not affect the original variable.
+            // b) is a class (Reference Type) Modifying the object through either variable will affect both,
+            // as they both point to the same instance.
+
+
+        }
+    }
+}
