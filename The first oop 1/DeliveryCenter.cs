1d29c28093b212b88a55a2e4d9e9f5a10408c4d4
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-11 03:28:17 +0300

    DeliveryCenter

--- The first oop 1/DeliveryCenter.cs
+++ The first oop 1/DeliveryCenter.cs
@@ -1,1 +1,67 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_first_oop_1
+{
+    public struct DeliveryCenter
+    {
+        private Shipment[] shipments;
+        private int count;
+
+
+        public DeliveryCenter()
+        {
+            shipments = new Shipment[10];
+            count = 0;
+        }
+        public Shipment this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= count)
+                {
+                    return default;
+                }
+                return shipments[index];
+            }
+            set
+            {
+                if (index >= 0 && index < count)
+                {
+                    shipments[index] = value;
+                }
+            }
+        }
+        public Shipment this[string trackingCode]
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(trackingCode))
+                    return default;
+
+                for (int i = 0; i < count; i++)
+                {
+                    if (shipments[i].trackingCode == trackingCode)
+                    {
+                        return shipments[i];
+
+                    }
+                }
+                return default;
+            }
+        }
+        // Method 
+        public bool AddShipment(Shipment shipment)
+        {
+            if (count < 10)
+            {
+                shipments[count] = shipment;
+                count++;
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
